bd0adb1f8b7d4d85414c5171a7a5dffef7b176ff
Jose Alejandro Rojano Crisson <jrojano@lean-tech.io>
2019-11-25 16:58:47 -0500

    Value component set http request and updated view.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { ValueComponent } from './value/value.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+    { path: '', component: ValueComponent },
+    { path: '**', redirectTo: '' },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/value/value.component.ts
+++ src/app/value/value.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Component({
-  selector: 'app-value',
-  templateUrl: './value.component.html',
-  styleUrls: ['./value.component.scss']
+    selector: 'app-value',
+    templateUrl: './value.component.html',
+    styleUrls: ['./value.component.scss'],
 })
 export class ValueComponent implements OnInit {
+    values: any;
 
-  constructor() { }
+    constructor(private http: HttpClient) {}
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this.getValues();
+    }
 
+    getValues() {
+        this.http.get(`${environment.apiUrl}/values`).subscribe(
+            (response: any) => {
+                this.values = response;
+            },
+            error => {
+                console.log(error);
+            }
+        );
+    }
 }
