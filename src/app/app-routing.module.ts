4b56b8dcf2fcd1c19b28f87973c6440632155dcb
Jose Alejandro Rojano Crisson <jrojano@lean-tech.io>
2020-01-07 16:49:28 -0500

    Added matches, lists and messages routing.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
 import { HomeComponent } from './home/home.component';
+import { ListsComponent } from './lists/lists.component';
+import { MemberListComponent } from './member-list/member-list.component';
+import { MessagesComponent } from './messages/messages.component';
 import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
-    { path: '', component: HomeComponent },
+    { path: '', component: HomeComponent, pathMatch: 'full' },
+    { path: 'lists', component: ListsComponent },
+    { path: 'members', component: MemberListComponent },
+    { path: 'messages', component: MessagesComponent },
     { path: 'register', component: RegisterComponent },
     { path: '**', redirectTo: '' },
 ];
