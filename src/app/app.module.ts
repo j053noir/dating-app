5fa99e0fa2647b1472025cf44d912b592de371a9
Jose Alejandro Rojano Crisson <jrojano@lean-tech.io>
2020-01-07 16:08:15 -0500

    Added BrowserAnimationsModule. Added BsDropdownModule.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
+
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { HomeComponent } from './home/home.component';
@@ -19,7 +24,14 @@ import { ErrorInterceptorProvider } from './_services/error.interceptor';
         HomeComponent,
         RegisterComponent,
     ],
-    imports: [BrowserModule, HttpClientModule, FormsModule, AppRoutingModule],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        BsDropdownModule.forRoot(),
+        HttpClientModule,
+        FormsModule,
+        AppRoutingModule,
+    ],
     providers: [AuthService, ErrorInterceptorProvider],
     bootstrap: [AppComponent],
 })
