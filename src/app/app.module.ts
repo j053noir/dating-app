d07ded70eec29e9b1400f6de3eb3f39081315182
Jose Alejandro Rojano Crisson <jrojano@lean-tech.io>
2020-01-14 09:36:59 -0500

    Added jwt Module to App Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,31 @@
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { JwtModule } from '@auth0/angular-jwt';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
-import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
+import { environment } from 'src/environments/environment';
 
 import { AppRoutingModule } from './app-routing.module';
 
-import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
-
 import { AppComponent } from './app.component';
-import { NavComponent } from './nav/nav.component';
 import { HomeComponent } from './home/home.component';
+import { ListsComponent } from './lists/lists.component';
+import { MemberCardComponent } from './members/member-card/member-card.component';
+import { MemberListComponent } from './members/member-list/member-list.component';
+import { MessagesComponent } from './messages/messages.component';
+import { NavComponent } from './nav/nav.component';
 import { RegisterComponent } from './register/register.component';
 
 import { AuthService } from './_services/auth.service';
 import { ErrorInterceptorProvider } from './_services/error.interceptor';
-import { MemberListComponent } from './members/member-list/member-list.component';
-import { ListsComponent } from './lists/lists.component';
-import { MessagesComponent } from './messages/messages.component';
 import { UserService } from './_services/user.service';
-import { MemberCardComponent } from './members/member-card/member-card.component';
+
+export function tokenGetter() {
+    return localStorage.getItem(environment.tokenName);
+}
 
 @NgModule({
     declarations: [
@@ -40,6 +45,13 @@ import { MemberCardComponent } from './members/member-card/member-card.component
         HttpClientModule,
         FormsModule,
         AppRoutingModule,
+        JwtModule.forRoot({
+            config: {
+                tokenGetter,
+                whitelistedDomains: [environment.hostUrl],
+                blacklistedRoutes: [`${environment.apiUrl}/auth`],
+            },
+        }),
     ],
     providers: [AuthService, UserService, ErrorInterceptorProvider],
     bootstrap: [AppComponent],
