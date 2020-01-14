518b8322a075f19c437ab9ce9b2e9980be9a0d3b
Jose Alejandro Rojano Crisson <jrojano@lean-tech.io>
2020-01-14 11:37:28 -0500

    Added photo gallery to member detail

--- src/app/members/member-detail/member-detail.component.ts
+++ src/app/members/member-detail/member-detail.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import {
+    NgxGalleryAnimation,
+    NgxGalleryImage,
+    NgxGalleryOptions,
+} from 'ngx-gallery';
 import { Subscription } from 'rxjs';
 import { User } from 'src/app/_models/user';
 
@@ -9,22 +14,49 @@ import { User } from 'src/app/_models/user';
     styleUrls: ['./member-detail.component.scss'],
 })
 export class MemberDetailComponent implements OnInit, OnDestroy {
-    userId: number;
     user: User;
     routeSubscription: Subscription;
-    userSubscription: Subscription;
+    galleryOptions: NgxGalleryOptions[];
+    galleryImages: NgxGalleryImage[];
 
-    constructor(private route: ActivatedRoute) {
+    constructor(private route: ActivatedRoute) {}
+
+    ngOnInit() {
         this.routeSubscription = this.route.data.subscribe(data => {
             this.user = data.user;
         });
-    }
 
-    ngOnInit() {}
+        this.galleryOptions = [
+            {
+                width: '500px',
+                height: '500px',
+                imagePercent: 100,
+                thumbnailsColumns: 4,
+                imageAnimation: NgxGalleryAnimation.Slide,
+                preview: false,
+            },
+        ];
+
+        this.galleryImages = this.getImages();
+    }
 
     ngOnDestroy() {
         if (this.routeSubscription) {
             this.routeSubscription.unsubscribe();
         }
+    }
+
+    getImages() {
+        const imagesUrls = [];
+        for (const photo of this.user.photos) {
+            imagesUrls.push({
+                small: photo.url,
+                medium: photo.url,
+                big: photo.url,
+                description: photo.description,
+            });
+        }
+
+        return imagesUrls;
     }
 }
