a39a878e7bd02275f8b8a73d976278f1f21315a1
Jose Alejandro Rojano Crisson <jrojano@lean-tech.io>
2020-01-17 10:16:29 -0500

    Set localstoage item "user" to store current user

--- src/app/_services/auth.service.ts
+++ src/app/_services/auth.service.ts
@@ -1,8 +1,10 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 import { JwtHelperService } from '@auth0/angular-jwt';
 import { map } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
+import { User } from '../_models/user';
 
 @Injectable({
     providedIn: 'root',
@@ -11,11 +13,18 @@ export class AuthService {
     baseUrl = `${environment.apiUrl}/auth`;
     jwtHelper = new JwtHelperService();
     decodedToken: any;
+    currentUser: User;
 
-    constructor(private http: HttpClient) {
+    constructor(private http: HttpClient, private router: Router) {
         const token = localStorage.getItem(environment.tokenName);
         if (token && this.loggedIn) {
             this.decodedToken = this.jwtHelper.decodeToken(token);
+
+            const user = localStorage.getItem(environment.userObjName);
+
+            if (user) {
+                this.currentUser = JSON.parse(user) as User;
+            }
         }
     }
 
@@ -24,9 +33,14 @@ export class AuthService {
             map((response: any) => {
                 if (response) {
                     localStorage.setItem(environment.tokenName, response.token);
+                    localStorage.setItem(
+                        environment.userObjName,
+                        JSON.stringify(response.user)
+                    );
                     this.decodedToken = this.jwtHelper.decodeToken(
                         response.token
                     );
+                    this.currentUser = response.user;
                     console.log(this.decodedToken);
                 }
             })
@@ -35,6 +49,10 @@ export class AuthService {
 
     logout() {
         localStorage.removeItem(environment.tokenName);
+        localStorage.removeItem(environment.userObjName);
+        this.decodedToken = null;
+        this.currentUser = null;
+        this.router.navigate(['/']);
     }
 
     register(model: any) {
