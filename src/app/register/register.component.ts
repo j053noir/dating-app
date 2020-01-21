48eb9eb182b3c0be57cf265822c06f35225df230
Jose Alejandro Rojano Crisson <jrojano@lean-tech.io>
2020-01-21 16:33:58 -0500

    Added new fields to register form

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,33 +1,92 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
-import { AuthService } from '../_services/auth.service';
+import {
+    Component,
+    EventEmitter,
+    OnDestroy,
+    OnInit,
+    Output,
+} from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Subscription } from 'rxjs';
 import { AlertifyService } from '../_services/alertify.service';
+import { AuthService } from '../_services/auth.service';
+import { BsDatepickerConfig } from 'ngx-bootstrap';
 
 @Component({
     selector: 'app-register',
     templateUrl: './register.component.html',
     styleUrls: ['./register.component.scss'],
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent implements OnInit, OnDestroy {
     @Output() cancelRegister = new EventEmitter();
-
+    registerSubscription: Subscription;
     model: any = {};
+    registerForm: FormGroup;
+    bsConfig: Partial<BsDatepickerConfig>;
 
     constructor(
         private authService: AuthService,
-        private alertifyService: AlertifyService
+        private alertifyService: AlertifyService,
+        private formBuilder: FormBuilder
     ) {}
 
-    ngOnInit() {}
+    ngOnInit() {
+        const adultAge = new Date().setFullYear(new Date().getFullYear() - 18);
+        this.bsConfig = {
+            containerClass: 'theme-green',
+            maxDate: new Date(adultAge),
+        };
+        this.createRegisterForm();
+    }
+
+    ngOnDestroy() {
+        if (this.registerSubscription) {
+            this.registerSubscription.unsubscribe();
+        }
+    }
+
+    createRegisterForm() {
+        this.registerForm = this.formBuilder.group(
+            {
+                gender: ['female'],
+                username: ['', Validators.required],
+                knownAs: ['', Validators.required],
+                dateOfBirth: [null, Validators.required],
+                city: ['', Validators.required],
+                country: ['', Validators.required],
+                password: [
+                    '',
+                    [
+                        Validators.required,
+                        Validators.minLength(4),
+                        Validators.maxLength(8),
+                    ],
+                ],
+                confirmPassword: ['', Validators.required],
+            },
+            { validators: this.passwordMatchValidator }
+        );
+    }
+
+    passwordMatchValidator(formGroup: FormGroup) {
+        return formGroup.get('password').value ===
+            formGroup.get('confirmPassword').value
+            ? null
+            : { mismatch: true };
+    }
 
     register() {
-        this.authService.register(this.model).subscribe(
-            () => {
-                this.alertifyService.success('User registered successfully');
-            },
-            err => {
-                this.alertifyService.error(err);
-            }
-        );
+        this.registerSubscription = this.authService
+            .register(this.registerForm.value)
+            .subscribe(
+                () => {
+                    this.alertifyService.success(
+                        'User registered successfully'
+                    );
+                },
+                err => {
+                    this.alertifyService.error(err);
+                }
+            );
     }
 
     cancel() {
