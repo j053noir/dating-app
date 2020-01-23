86d5382f716552071045f0a3b703495d8d1bdbdd
Jose Alejandro Rojano Crisson <jrojano@lean-tech.io>
2020-01-23 15:20:06 -0500

    Added pagination to members list

--- src/app/members/member-list/member-list.component.ts
+++ src/app/members/member-list/member-list.component.ts
@@ -2,6 +2,9 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { User } from '../../_models/user';
+import { Pagination } from 'src/app/_models/pagination';
+import { UserService } from 'src/app/_services/user.service';
+import { AlertifyService } from 'src/app/_services/alertify.service';
 
 @Component({
     selector: 'app-member-list',
@@ -10,13 +13,20 @@ import { User } from '../../_models/user';
 })
 export class MemberListComponent implements OnInit, OnDestroy {
     users: User[];
+    pagination: Pagination;
     routeSubscription: Subscription;
+    usersSubscription: Subscription;
 
-    constructor(private route: ActivatedRoute) {}
+    constructor(
+        private route: ActivatedRoute,
+        private userService: UserService,
+        private alertify: AlertifyService
+    ) {}
 
     ngOnInit() {
         this.routeSubscription = this.route.data.subscribe(data => {
             this.users = data.users.result;
+            this.pagination = data.users.pagination;
         });
     }
 
@@ -24,5 +34,27 @@ export class MemberListComponent implements OnInit, OnDestroy {
         if (this.routeSubscription) {
             this.routeSubscription.unsubscribe();
         }
+        if (this.usersSubscription) {
+            this.usersSubscription.unsubscribe();
+        }
+    }
+
+    pageChanged(event: any) {
+        this.pagination.currentPage = event.page;
+        this.loadUsers();
+    }
+
+    loadUsers() {
+        this.usersSubscription = this.userService
+            .getUsers(this.pagination.currentPage, this.pagination.itemsPerPage)
+            .subscribe(
+                res => {
+                    this.users = res.result;
+                    this.pagination = res.pagination;
+                },
+                err => {
+                    this.alertify.error(err);
+                }
+            );
     }
 }
