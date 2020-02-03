541848e3848f1699992ecc6054e672160f0a1a52
Jose Alejandro Rojano Crisson <jrojano@lean-tech.io>
2020-02-03 16:24:34 -0500

    Created member message component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -32,6 +32,7 @@ import { MemberCardComponent } from './members/member-card/member-card.component
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 import { MemberEditComponent } from './members/member-edit/member-edit.component';
 import { MemberListComponent } from './members/member-list/member-list.component';
+import { MemberMessagesComponent } from './members/member-messages/member-messages.component';
 import { PhotoEditorComponent } from './members/photo-editor/photo-editor.component';
 import { MessagesComponent } from './messages/messages.component';
 import { NavComponent } from './nav/nav.component';
@@ -74,6 +75,7 @@ export class CustomHammerConfig extends HammerGestureConfig {
         MemberEditComponent,
         PhotoEditorComponent,
         TimeAgoPipe,
+        MemberMessagesComponent,
     ],
     imports: [
         BrowserModule,
---
+++ src/app/members/member-messages/member-messages.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit, Input, OnDestroy } from '@angular/core';
+import { Message } from 'src/app/_models/message';
+import { UserService } from 'src/app/_services/user.service';
+import { AuthService } from 'src/app/_services/auth.service';
+import { Subscription } from 'rxjs';
+import { AlertifyService } from 'src/app/_services/alertify.service';
+
+@Component({
+    selector: 'app-member-messages',
+    templateUrl: './member-messages.component.html',
+    styleUrls: ['./member-messages.component.scss'],
+})
+export class MemberMessagesComponent implements OnInit, OnDestroy {
+    @Input() recipientId: number;
+
+    messages: Message[] = [];
+    messagesSusbcription: Subscription;
+
+    constructor(
+        private authService: AuthService,
+        private userService: UserService,
+        private alertify: AlertifyService
+    ) {}
+
+    ngOnInit() {
+        this.loadMessages();
+    }
+
+    ngOnDestroy(): void {
+        throw new Error('Method not implemented.');
+    }
+
+    loadMessages() {
+        const userId = this.authService.decodedToken.nameid;
+        this.messagesSusbcription = this.userService
+            .getMessageThread(userId, this.recipientId)
+            .subscribe(
+                messages => {
+                    this.messages = messages;
+                },
+                err => {
+                    this.alertify.error(err);
+                }
+            );
+    }
+}
